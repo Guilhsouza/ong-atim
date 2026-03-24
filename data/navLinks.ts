1c65f0852fb43a947e95f94b2053f0bea32c642d
Guilherme Souza <gui.edu0380@gmail.com>
2026-03-24 15:06:46 -0300

    colocando navLinks para exportar para melhor escalonamento

---
+++ data/navLinks.ts
@@ -0,0 +1,14 @@
+type NavLink = {
+    label: string
+    href: string
+    type: "link" | "scroll"
+}
+
+const navLinks: NavLink[] = [
+    { label: "Relatos", href: "/relatos", type: "link" },
+    { label: "Doadores", href: "#sponsors", type: "scroll" },
+    { label: "Entidades Atendidas", href: "/projetos", type: "link" },
+    { label: "Contato", href: "#footer", type: "scroll" },
+]
+
+export default navLinks

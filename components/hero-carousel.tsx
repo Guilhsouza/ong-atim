08c52d42c7b14cbce2e9e1f24dc60327a7912e4a
Guilherme Souza <gui.edu0380@gmail.com>
2026-03-31 12:08:32 -0300

    alterando tamanho da logo

--- components/hero-carousel.tsx
+++ components/hero-carousel.tsx
@@ -36,15 +36,17 @@ export function HeroCarousel({ heroRef }: {
 
 
   return (
-    <section className="relative h-screen w-full overflow-hidden" ref={heroRef}>
+    <section className="relative h-screen w-full overflow-hidden flex " ref={heroRef}>
 
-      <Image
-        src={logoAtim}
-        alt="Logo ATIM"
-        fill
-        priority
-        className="object-contain"
-      />
+      <div className="relative w-150 h-150 2xl:w-200 2xl:h-200 mx-auto">
+        <Image
+          src={logoAtim}
+          alt="Logo ATIM"
+          fill
+          priority
+          className="object-contain"
+        />
+      </div>
 
       <div className="invisible md:visible">
         <HeroMenu

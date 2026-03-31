25eacfc6876cf0ae0cd144eddd53bf21c152a5c6
Guilherme Souza <gui.edu0380@gmail.com>
2026-03-31 13:46:15 -0300

    centralizando logo

--- components/hero-carousel.tsx
+++ components/hero-carousel.tsx
@@ -38,7 +38,7 @@ export function HeroCarousel({ heroRef }: {
   return (
     <section className="relative h-screen w-full overflow-hidden flex " ref={heroRef}>
 
-      <div className="relative w-150 h-150 2xl:w-200 2xl:h-200 mx-auto">
+      <div className="relative w-150 h-full 2xl:w-200 mx-auto">
         <Image
           src={logoAtim}
           alt="Logo ATIM"
